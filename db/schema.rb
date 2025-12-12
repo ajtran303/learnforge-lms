f3e69857440361e488f74c69f7751ab4dd88ef82
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-12 02:23:50 -0500

    Update course to have default draft status

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_12_062154) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_12_071107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
+    t.string "status", default: "draft"
     t.string "title"
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/courses.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :course do
+    title { "Intro to Ruby" }
+    description { "Learn Ruby basics" }
+  end
+end
