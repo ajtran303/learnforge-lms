6933784b16dd959f8b39776f589939a0b517ea03
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-12 01:32:03 -0500

    Add basic learner rbac flow

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,13 @@
 class CoursesController < ApplicationController
+  before_action :require_instructor
+
   def new
     @course = Course.new
+  end
+
+  private
+
+  def require_instructor
+    render plain: "Forbidden", status: :forbidden unless current_user&.role == "instructor"
   end
 end
