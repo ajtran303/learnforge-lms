e2dc0503a89eb971fa499dc82ecbbc32741e1619
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-12 01:54:10 -0500

    Add course management spec

---
+++ spec/system/course_management_spec.rb
@@ -0,0 +1,31 @@
+reqeuire "rails_helper"
+
+RSpec.describe "Course Management", type: :system do
+  let!(:instructor) { FactoryBot.create(:user, role: "instructor") }
+
+  before do
+    driven_by(:rack_test)
+    login_as(instructor)
+  end
+
+  it "allows an instructor to create a course with valid details" do
+    visit new_course_path
+
+    fill_in "Title", with: "Intro to Ruby"
+    fill_in "Description", with: "Learn Ruby basics"
+    click_button "Create Course"
+
+    expect(page).to have_current_path(course_path(Course.last))
+    expect(page).to have_content("Course created successfully")
+    expect(Course.last.status).to eq("draft")
+  end
+
+  it "shows validation errors when invalid" do
+    fill_in "Title", with: ""
+    fill_in "Description", with: ""
+    click_button "Create Course"
+
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Description can't be blank")
+  end
+end
