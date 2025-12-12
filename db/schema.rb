1a881b1e977b9d91eebc89cf0d1c32df11a523b0
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-12 06:28:42 -0500

    Add lessons; courses have many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_12_073435) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_12_110936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_073435) do
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
+  create_table "lessons", force: :cascade do |t|
+    t.text "content"
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", null: false
@@ -34,4 +43,5 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_073435) do
   end
 
   add_foreign_key "courses", "users"
+  add_foreign_key "lessons", "courses"
 end
