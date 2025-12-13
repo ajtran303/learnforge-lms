33e29d14907e2daf2d6f6e1e5bdf2421b5052148
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-12 20:57:40 -0500

    Add associations

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Lesson, type: :model do
     it { should validate_presence_of(:content) }
 
     it { should belong_to :course }
+
+    it { should have_many(:completed_by_users).through(:lesson_completions).source(:user) }
 end
