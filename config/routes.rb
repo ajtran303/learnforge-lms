bd63925b50a3ce6824b2fc481de3eaad1590b195
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-13 15:15:40 -0500

    Change root to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "dashboard#show"
+
   get "/sign_up", to: "users#new", as: :new_user_registration
   post "/users", to: "users#create"
 
