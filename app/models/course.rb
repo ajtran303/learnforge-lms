fe2bdc15d707ceda1ff69cc1dec07eb0063bc623
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-12 19:30:48 -0500

    Add lesson viewer with nav

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
-  has_many :lessons
+  has_many :lessons, dependent: :destroy
   has_and_belongs_to_many :enrolled_users,
     class_name: "User",
     join_table: "courses_users",
