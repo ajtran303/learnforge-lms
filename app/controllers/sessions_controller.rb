f93f964664841a994dfaa81ff88b927d7b54e7e3
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-17 00:04:06 -0500

    Fix lesson navigation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to dashboard_path, notice: "Logged in successfully!"
+      redirect_to dashboard_path, alert: "Logged in successfully!"
     else
       flash.now[:alert] = "Invalid email or password"
       render :new, status: :unprocessable_entity
